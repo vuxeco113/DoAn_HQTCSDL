ed5cb35ad60e24bdaaceae9e5b3985244bfb6ff4
LiemPhong <liemphongiv666@gmail.com>
2024-12-09 22:57:14 +0700

    update Nguyen

--- DoAnHQTCSDL/GUI/Program.cs
+++ DoAnHQTCSDL/GUI/Program.cs
@@ -16,7 +16,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_Nguyen_XuatVien());
+            Application.Run(new frmMain());
 
         }
     }
